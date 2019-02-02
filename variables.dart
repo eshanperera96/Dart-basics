26da2b443a6a2bb9e1c8c3f323d3a18a73f65383
eshanperera96 <47264828+eshanperera96@users.noreply.github.com>
2019-02-02 09:43:56 +0530

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main(){
+int score = 23;
+	var count = 23;     // It is inferred as integer automatically by compiler
+	int hexValue = 0xEADEBAEE;
+
+	// Numbers: double
+	double percentage = 93.4;
+	var percent = 82.533;
+	double exponents = 1.42e5; 
+
+	// Strings
+	String name = "Henry";
+	var company = "Google";
+
+	// Boolean
+	bool isValid = true;
+	var isAlive = false;
+
+	print(score);
+	
+}
