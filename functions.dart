199c2370d23d2d338e8fa528a1600192ea206240
eshanperera96 <47264828+eshanperera96@users.noreply.github.com>
2019-02-02 10:37:43 +0530

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,20 @@
+void main()
+{
+ 
+  findmultiplication();
+  findarea(10,5);
+}
+void findmultiplication()
+{
+  int a=2;
+  int b=3;
+  
+  int multi= a*b;
+  print("answer is $multi");
+}
+ 
+int findarea(int width,int height)
+{
+ int area= width*height;
+  print ("area=$area");
+}
